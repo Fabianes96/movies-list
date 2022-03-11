e881ce979f7d6bc17c4efb125c647db2581e0e3e
Fabian Higuita <fabianes1996@gmail.com>
2022-03-11 18:49:06 -0500

    Add initial section content

--- src/components/InitialSection/InitialSection.js
+++ src/components/InitialSection/InitialSection.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import './InitialSection.css'
 
 const InitialSection = () => {
-  return (
-    <div>InitialSection</div>
+  return(
+    <div className="home-initial">
+      <div className='home-initial_text'>
+        <h1>Welcome to the Movies List App</h1>
+        <p>Find now the best movies, take a look!</p>
+
+      </div>
+    </div>
   )
 }
 
