e2b2f38b779dc3d844f213fd2d9829735644fedd
Fabian Higuita <fabianes1996@gmail.com>
2022-03-11 19:07:08 -0500

    Update movieDetail

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Carrousel from "../Carrousel/Carrousel";
 import "./MovieDetail.css";
+import moviePath from "../../assets/movie_img.png";
 import EventIcon from "@mui/icons-material/Event";
 import StarBorderIcon from "@mui/icons-material/StarBorder";
 import { yellow } from "@mui/material/colors";
@@ -8,26 +9,34 @@ const MovieDetail = (props) => {
   const { images, movie } = props;
   const { genres } = movie;
   const castArray = props.cast.cast;
+  let path = "";
+  if (movie.poster_path || movie.backdrop_path) {
+    if (movie.poster_path) {
+      path = images.base_url + images.poster_sizes[4] + movie.poster_path;
+    } else {
+      path = images.base_url + images.poster_sizes[4] + movie.backdrop_path;
+    }
+  } else {
+    path = moviePath;
+  }
   return (
-    <div className="container-details">
-      <div className="container-details-poster">
-        <img
-          src={images.base_url + images.poster_sizes[4] + movie.poster_path}
-          alt={movie.title}
-        />
-        <span>{movie?.tagline}</span>
-        <div className="container-details-poster-icons">
-          <div className="star">
-            <StarBorderIcon sx={{ color: yellow[500] }} fontSize="small" />
-            <span>{movie?.vote_average}</span>
-          </div>
-          <div className="date">
-            <EventIcon fontSize="small" />
-            <span>{movie?.release_date}</span>
+    <section id="movie-detail">
+      <div className="container-details">
+        <div className="container-details-poster">
+          <img src={path} alt={movie.title} />
+          <span>{movie?.tagline}</span>
+          <div className="container-details-poster-icons">
+            <div className="star">
+              <StarBorderIcon sx={{color:yellow[500]}} fontSize="small" />
+              <span>{movie?.vote_average}</span>
+            </div>
+            <div className="date">
+              <EventIcon fontSize="small" />
+              <span>{movie?.release_date}</span>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="container-movie-info">
+        <div className="container-movie-info">
           <div className="container-movie-info__header">
             <h2>{movie.title}</h2>
             <div className="div-genres">
@@ -39,21 +48,24 @@ const MovieDetail = (props) => {
             </div>
             <p>{movie.overview}</p>
           </div>
-        {
-          <Carrousel
-            items={castArray}
-            conf={images}
-            path={"profile_path"}
-            loop={false}
-            effect={""}
-            autoplay={false}
-            className={"swiper-slide-cast"}
-            pagination={false}
-            classNameSwiper={"swiper-cast"}
-          />
-        }
+          {
+            <Carrousel
+              items={castArray}
+              conf={images}
+              path={"profile_path"}
+              loop={false}
+              effect={""}
+              autoplay={false}
+              className={"swiper-slide-cast"}
+              cover={false}
+              pagination={false}
+              classNameSwiper={"swiper-cast"}
+              title={"Cast"}
+            />
+          }
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
